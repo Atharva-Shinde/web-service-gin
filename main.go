aebdc6028b47028e416df0360634c0e2e13978d6
Atharva Shinde <atharvashinde179@gmail.com>
2022-03-02 02:30:32 +0530

    updated: main.go

--- main.go
+++ main.go
@@ -6,43 +6,44 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// album represents the structure of the data of each album
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+// cover represents the structure of the data of each cover
+type cover struct {
+	ID               string  `json:"id"`
+	Title            string  `json:"title"`
+	Artist           string  `json:"artist"`
+	PremierMonthYear float64 `json:"premier"`
 }
 
 // slice which contains the data of each album
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+var covers = []cover{
+	{ID: "1", Title: "Interstellar", Artist: "Hans Zimmer", PremierMonthYear: 10.2014},
+	{ID: "2", Title: "Building Light", Artist: "Anne Sophie Versnaeyen", PremierMonthYear: 2018},
+	{ID: "3", Title: "Zen", Artist: "Torin Borrowdale", PremierMonthYear: 03.2018},
+	{ID: "4", Title: "Married Life", Artist: "Jason Lyle Black", PremierMonthYear: 10.2015},
 }
 
 func main() {
 	// to initialise gin router
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
+	router.GET("/covers", getCovers)
+	router.POST("/covers", postCovers)
 
 	router.Run("localhost:8080")
 
 }
 
 // gin.Context carries request details, validates and serializes JSON, and more
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getCovers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, covers)
 }
 
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+func postCovers(c *gin.Context) {
+	var newCover cover
 
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err := c.BindJSON(&newCover); err != nil {
 		return
 	}
 
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, albums)
+	covers = append(covers, newCover)
+	c.IndentedJSON(http.StatusCreated, covers)
 }
